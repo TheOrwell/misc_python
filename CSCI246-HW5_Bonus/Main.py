f757e1e7f89321d010d9d1a31a6e61b9a1ed7130
Isaac Yauk <isaacyauk@yahoo.com>
2024-04-04 12:58:38 -0600

    inital commit

---
+++ CSCI246-HW5_Bonus/Main.py
@@ -0,0 +1,18 @@
+"""
+TODO: Implement RSA encryption and decryption (see page 374 of the textbook).
+    This can be encapsulated in three simple functions:
+
+        - GenerateKeys(p, q): returns (d,e), where p and q are different primes and (d, pq) forms the private key and
+            (e, pq) is the public key.
+        - Encrypt(M, e, pq): returns E, the encrypted version of M (M is an int < pq)
+        - Decrypt(E, d, pq): returns M, the decrypted version of E
+
+    Demonstrate your code works on some examples with different primes and messages.
+
+TODO: Extend your code to handle strings.
+
+Code should be well-styled (commenting, spacing, easily run), and uploaded.
+Also include a screenshot of the code running.
+"""
+
+print("Hello world!")
