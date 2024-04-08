739433ec8dc98a98dd96c9aa676a0dbfaf33ea09
Isaac Yauk <isaacyauk@yahoo.com>
2024-04-08 12:03:20 -0600

    adds functioning euclid method; debugging test cases included!

--- CSCI246-HW5_Bonus/Main.py
+++ CSCI246-HW5_Bonus/Main.py
@@ -15,4 +15,33 @@ Code should be well-styled (commenting, spacing, easily run), and uploaded.
 Also include a screenshot of the code running.
 """
 
-print("Hello world!")
+
+def generateKeys(p, q):
+    print("Method in development")
+    # return (d, e)
+
+
+def encrypt(M, e, pq):
+    print("Method in development")
+    # return E
+
+
+def decrypt(E, d, pq):
+    print("Method in development")
+    # return M
+
+
+def gcd(m, n):
+    return n % m
+
+
+def euclid(n, m):
+    if m % n == 0:
+        return n
+    else:
+        return euclid(m % n, n)
+
+
+print(euclid(17, 42))
+print(euclid(48, 1024))
+
