1f5d963eba8920f6f5ac52378a1f6979412a3588
Isaac Yauk <isaacyauk@yahoo.com>
2024-04-16 13:27:48 -0600

    gets working solution

--- CSCI246-HW5_Bonus/Main.py
+++ CSCI246-HW5_Bonus/Main.py
@@ -1,38 +1,52 @@
-"""
-TODO: Implement RSA encryption and decryption (see page 374 of the textbook).
-    This can be encapsulated in three simple functions:
+def GenerateKeys(p, q):
+    # Choosing 2 large primes p and q in method call below
+    # Define n == pq
+    n = p * q
 
-        - GenerateKeys(p, q): returns (d,e), where p and q are different primes and (d, pq) forms the private key and
-            (e, pq) is the public key.
-        - Encrypt(M, e, pq): returns E, the encrypted version of M (M is an int < pq)
-        - Decrypt(E, d, pq): returns M, the decrypted version of E
+    # choose an e != 1 so that (p-1) and (q-1) are relatively prime
+    relPrime = False
+    e = 2  # Choosing an e != 1
+    while not relPrime:
+        # Check if e is relatively prime with (p-1)*(q-1)
+        _, _, gcd = extEuclid(e, (p - 1) * (q - 1))
+        if gcd == 1:
+            relPrime = True
+        else:
+            e += 1
 
-    Demonstrate your code works on some examples with different primes and messages.
+    # Compute inverse of e % (p-1)(q-1) and make it equal to d.
+    d = inverse(e, (p - 1) * (q - 1))
 
-TODO: Extend your code to handle strings.
-
-Code should be well-styled (commenting, spacing, easily run), and uploaded.
-Also include a screenshot of the code running.
-"""
-
-
-def generateKeys(p, q):
-    print("Method in development")
-    # return (d, e)
-
-
-def encrypt(M, e, pq):
-    print("Method in development")
-    # return E
+    # Publish Public key: <e, n> Secret key: <d,n>
+    return (e, n), (d, n)
 
 
-def decrypt(E, d, pq):
-    print("Method in development")
-    # return M
+def Encrypt(M, e, n):
+    # Compute modular exponentiation using the built-in pow method and return result
+    return pow(M, e, n)
 
 
-def gcd(m, n):
-    return n % m
+def Decrypt(E, d, n):
+    # Compute modular exponentiation using the built-in pow method, with adjusted vars for decryption and return result
+    return pow(E, d, n)
+
+
+# inverse function from previous homeworks
+def inverse(a, n):
+    x, y, d = extEuclid(a, n)
+    if d == 1:
+        return x % n
+    else:
+        return f"No inverse for a exists in ℤ_{n}"
+
+
+# extEuclid function from previous homeworks
+def extEuclid(n, m):
+    if m % n == 0:
+        return 1, 0, n
+    else:
+        x, y, r = extEuclid(m % n, n)
+        return (y - (m // n) * x), x, r
 
 
 def euclid(n, m):
@@ -42,6 +56,20 @@ def euclid(n, m):
         return euclid(m % n, n)
 
 
-print(euclid(17, 42))
-print(euclid(48, 1024))
+# Example from book, run via code:
+# Generate keys
+p, q = 13, 17
+public_key, private_key = GenerateKeys(p, q)
+print(f"Public key: {public_key}")
+print(f"Private key: {private_key}")
 
+# Encrypt message
+m = 202
+e, n = public_key
+encrypted = Encrypt(m, e, n)
+print(f"Encrypted message: {encrypted}")
+
+# Decrypt ciphertext
+d, n = private_key
+decrypted = Decrypt(encrypted, d, n)
+print(f"Decrypted message: {decrypted}")
